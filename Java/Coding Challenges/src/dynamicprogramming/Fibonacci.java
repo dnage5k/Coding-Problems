54313920b57b24e03ca735e53c5e4f8b1a3925df
Dennis <crazydennis2002@yahoo.com>
2020-12-06 23:58:06 -0800

    Updated fib program. Thought it was correct

--- Java/Coding Challenges/src/dynamicprogramming/Fibonacci.java
+++ Java/Coding Challenges/src/dynamicprogramming/Fibonacci.java
@@ -5,7 +5,7 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int fib1 = 2;
-		int fib2 = 7;
+		int fib2 = 3;
 		int fib3 = 4;
 		
 //		System.out.println(fib(fib1));
@@ -22,14 +22,14 @@ public class Fibonacci {
 			return 1;
 		}
 		
-		int[] arr = new int[n];
+		int[] arr = new int[n+1];
 		arr[0] = 0; arr[1] = 1;
 		
-		for(int i = 2; i < n; i++) {
+		for(int i = 2; i < n+1; i++) {
 			arr[i] = arr[i-1] + arr[i-2];
 		}
 		
-		return arr[n-1];
+		return arr[n];
 	}
 
 }
