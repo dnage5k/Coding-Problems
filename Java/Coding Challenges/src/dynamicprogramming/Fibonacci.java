b8e1e63ec42fb47db933b66fa10e1464fe1cbd4a
Dennis <crazydennis2002@yahoo.com>
2020-12-10 04:13:47 -0800

    Finished Dynamic approach on fib

--- Java/Coding Challenges/src/dynamicprogramming/Fibonacci.java
+++ Java/Coding Challenges/src/dynamicprogramming/Fibonacci.java
@@ -5,11 +5,12 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int fib1 = 2;
-		int fib2 = 3;
+		int fib2 = 7;
 		int fib3 = 4;
+		int memo[] = new int[fib2+1];
 		
 //		System.out.println(fib(fib1));
-		System.out.println(fibM(fib2));
+		System.out.println(fibM(fib2,memo));
 //		System.out.println(fib(fib3));
 	}
 	
@@ -32,16 +33,24 @@ public class Fibonacci {
 		return arr[n];
 	}
 	
-	public static int fibM(int n) {
-		int[] memo = new int[n];
+	public static int fibM(int n, int[] memo) {
+		
+		memo[0] = 0;
+		memo[1] = 1;
+		
 		if (n == 0) {
 			return 0;
-		}else if(n <= 2) {
-			return 1;
-		}else {
-			memo[n] = fibM(n-1) + fibM(n-2);
+		}else if(n < 2) {
+			return n;
+		}else if(memo[n] != 0) {
 			return memo[n];
+		}else {
+			int f = fibM(n-1, memo) + fibM(n-2,memo);
+			memo[n] = f;
+			return f;
+			
 		}
+		
 		
 	}
 
