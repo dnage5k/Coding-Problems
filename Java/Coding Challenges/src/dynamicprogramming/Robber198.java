55f67e5acc95c4940d32a379221b841c145e2b09
Dennis <crazydennis2002@yahoo.com>
2020-12-12 07:01:15 -0800

    Finished the memo method but will approach the iterative way

--- Java/Coding Challenges/src/dynamicprogramming/Robber198.java
+++ Java/Coding Challenges/src/dynamicprogramming/Robber198.java
@@ -3,8 +3,9 @@ package dynamicprogramming;
 public class Robber198 {
 
 	public static void main(String[] args) {
-		int[] nums = {1,2,3,1};
-		System.out.println(rob(nums));
+		int[] nums = {3,2,3,100};
+		int[] memo = new int[nums.length];
+		System.out.println(robMemo(nums,memo, nums.length-1));
 
 	}
 	public static int rob(int[] nums) {
@@ -15,5 +16,17 @@ public class Robber198 {
 	        return 0;
 	    }
 	    return Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
+	}
+	
+	public static int robMemo(int[] nums, int[] memo, int i) {
+		if(i < 0) {
+			return 0;
+		}else if(memo[i] != 0) {
+			return memo[i];
+		}
+		int m = Math.max(robMemo(nums,memo,i-2) + nums[i], robMemo(nums,memo,i-1) );
+		memo[i] = m;
+		return m;
+		
 	}
 }
