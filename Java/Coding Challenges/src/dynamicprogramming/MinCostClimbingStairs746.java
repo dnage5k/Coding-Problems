6021c8224687bce3889979d5e10108d3399d7470
Dennis <crazydennis2002@yahoo.com>
2020-12-18 05:21:12 -0800

    Finished memoization for min cost climbing stairs

--- Java/Coding Challenges/src/dynamicprogramming/MinCostClimbingStairs746.java
+++ Java/Coding Challenges/src/dynamicprogramming/MinCostClimbingStairs746.java
@@ -4,7 +4,9 @@ public class MinCostClimbingStairs746 {
 
 	public static void main(String[] args) {
 		int[] arr = {10,15,20};
+		int[] memo = new int[arr.length];
 		System.out.println(Math.min(recursive(arr,arr.length-2), recursive(arr,arr.length-1)));
+		System.out.println(Math.min(recursiveMemo(arr,arr.length-2, memo), recursiveMemo(arr,arr.length-1, memo)));
 
 	}
 	
@@ -17,6 +19,20 @@ public class MinCostClimbingStairs746 {
 		}
 		
 		return cost[i] + Math.min(recursive(cost, i-2), recursive(cost,i-1));
+	}
+	
+	public static int recursiveMemo(int[] cost, int i, int[] memo) {
+		if(i < 0) {
+			return 0;
+		}else if(i == 1 || i == 0) {
+			return cost[i];
+			
+		}else if(memo[i] != 0) {
+			return memo[i];
+		}
+		int a = cost[i] + Math.min(recursiveMemo(cost,i-2,memo), recursiveMemo(cost,i-1,memo));
+		memo[i] = a;
+		return a;
 	}
 
 }
