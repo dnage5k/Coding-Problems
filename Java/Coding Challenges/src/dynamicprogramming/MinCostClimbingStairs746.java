2bab83208b3e8e14fdac4b5e97945b270d5236ed
Dennis <crazydennis2002@yahoo.com>
2020-12-18 23:00:18 -0800

    iterative memo method for min cost step problem

--- Java/Coding Challenges/src/dynamicprogramming/MinCostClimbingStairs746.java
+++ Java/Coding Challenges/src/dynamicprogramming/MinCostClimbingStairs746.java
@@ -7,6 +7,7 @@ public class MinCostClimbingStairs746 {
 		int[] memo = new int[arr.length];
 		System.out.println(Math.min(recursive(arr,arr.length-2), recursive(arr,arr.length-1)));
 		System.out.println(Math.min(recursiveMemo(arr,arr.length-2, memo), recursiveMemo(arr,arr.length-1, memo)));
+		System.out.println(iterativeMemo(arr));
 
 	}
 	
@@ -33,6 +34,22 @@ public class MinCostClimbingStairs746 {
 		int a = cost[i] + Math.min(recursiveMemo(cost,i-2,memo), recursiveMemo(cost,i-1,memo));
 		memo[i] = a;
 		return a;
+	}
+	
+	public static int iterativeMemo(int[] cost) {
+		int[] memo = new int[cost.length];
+		if(cost.length == 2) {
+			return Math.min(cost[0], cost[1]);
+		}
+		
+		memo[0] = cost[0];
+		memo[1] = cost[1];
+		for(int i = 2; i <cost.length; i++ ) {
+			int a = cost[i] + Math.min(memo[i-2], memo[i-1]);
+			memo[i] = a;
+		}
+		return Math.min(memo[cost.length-1], memo[cost.length-2]);
+		
 	}
 
 }
