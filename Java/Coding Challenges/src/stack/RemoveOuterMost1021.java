890e187144089933207e427ecec5c5d5866b0e9d
Dennis <crazydennis2002@yahoo.com>
2020-12-27 23:52:55 -0800

    Working on stack problem

---
+++ Java/Coding Challenges/src/stack/RemoveOuterMost1021.java
@@ -0,0 +1,12 @@
+package stack;
+import java.util.*;
+public class RemoveOuterMost1021 {
+
+	public static void main(String[] args) {
+		StringBuilder s2 = new StringBuilder();
+		s2.append('(');
+		System.out.println(s2.charAt(0));
+
+	}
+
+}
