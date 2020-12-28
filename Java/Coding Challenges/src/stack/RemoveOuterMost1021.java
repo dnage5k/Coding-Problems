ffd99cc0cf9ac611e99785158df00efe71661792
Dennis <crazydennis2002@yahoo.com>
2020-12-28 03:55:18 -0800

    Finished outer problem. Need to think out the problem more, dont look into the same solution too long.

--- Java/Coding Challenges/src/stack/RemoveOuterMost1021.java
+++ Java/Coding Challenges/src/stack/RemoveOuterMost1021.java
@@ -1,12 +1,55 @@
 package stack;
 import java.util.*;
 public class RemoveOuterMost1021 {
-
+// 		problem
+	
+// 		Input: "(()())(())"
+//		Output: "()()()"
+//		Explanation: 
+//		The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+//		After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+//		Example 2:
+//
+//		Input: "(()())(())(()(()))"
+//		Output: "()()()()(())"
+//		Explanation: 
+//		The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+//		After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+//		Example 3:
+//
+//		Input: "()()"
+//		Output: ""
+//		Explanation: 
+//		The input string is "()()", with primitive decomposition "()" + "()".
+//		After removing outer parentheses of each part, this is "" + "" = "".
+	
+	
 	public static void main(String[] args) {
-		StringBuilder s2 = new StringBuilder();
-		s2.append('(');
-		System.out.println(s2.charAt(0));
+		String s = "(())()()((())())";
+		System.out.println(sol(s));
 
+	}
+	
+	public static String sol(String s) {
+		StringBuilder s2 = new StringBuilder();
+		Stack<Character> stk = new Stack<>();
+		
+		for(char c : s.toCharArray()) {
+			if(c == '(') {
+				if(!stk.empty()) {
+					s2.append(c);
+				}
+				stk.push(c);
+				
+			}else{
+				stk.pop();
+				if(!stk.empty()) {
+					s2.append(c);
+				}
+			}
+		}
+		
+		return s2.toString();
 	}
 
 }
