36f95569038fbdb02ff46fda019c305b7ee365c6
Dennis <crazydennis2002@yahoo.com>
2020-12-31 21:08:15 -0800

    Found better method using hashmap

--- Java/Coding Challenges/src/LeetCode/FindAllDups442.java
+++ Java/Coding Challenges/src/LeetCode/FindAllDups442.java
@@ -20,5 +20,22 @@ public class FindAllDups442 {
         }
         return ans;
 	}
+	
+	public static List<Integer> mapMethod(int[] nums){
+		Map<Integer, Integer> map = new HashMap<>();
+		List<Integer> ans = new ArrayList<>();
+		
+		for(int i : nums) {
+			map.put(i, map.getOrDefault(i,0) +1);
+		}
+		
+		for(int i: map.keySet()) {
+			if(map.get(i) > 1) {
+				ans.add(i);
+			}
+		}
+		
+		return ans;
+	}
 
 }
