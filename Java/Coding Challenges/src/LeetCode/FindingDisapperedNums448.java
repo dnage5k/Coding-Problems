27294d3a2fd9f2ae176e744cf9618e5553638041
Dennis <crazydennis2002@yahoo.com>
2021-01-01 22:26:51 -0800

    Finished leetcode 448

--- Java/Coding Challenges/src/LeetCode/FindingDisapperedNums448.java
+++ Java/Coding Challenges/src/LeetCode/FindingDisapperedNums448.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class FindingDisapperedNums448 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int nums[] = {4,3,2,7,8,2,3,1};
+		System.out.println(bestMethod(nums));
 
 	}
 	
@@ -25,6 +26,30 @@ public class FindingDisapperedNums448 {
         }
         
         return list;
+	}
+	
+	// Method
+	// Similar to the Find all dup problems method
+	// loop through the original array and grab each index of nums[Math.abs(i) -1] to see if that number is negative or not
+	// If it is not negative then change that number of that index to negative
+	// The idea is that all the number visited will be negative while the ones arent visited are positive
+	// The number(s) is/are positive when it doesnt appear since there were no number in that array to reach that index
+	// if that number at that index is positive that means we add it to the list, the index and not the value of the index.
+	public static List<Integer> bestMethod(int[] nums){
+		 List<Integer> list = new ArrayList<>();
+	        for(int i : nums){
+	            if(nums[Math.abs(i)-1] > 0){
+	                nums[Math.abs(i)-1] *= -1;
+	            }
+	        }
+	        
+	       
+	        for(int i = 0; i <= nums.length-1;i++){
+	            if(nums[i] > 0){
+	                list.add(i+1);
+	            }
+	        }
+	        return list;
 	}
 
 }
