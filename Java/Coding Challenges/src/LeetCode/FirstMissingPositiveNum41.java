785b3497d966813e221236e1ba39fbcf67879db7
Dennis <crazydennis2002@yahoo.com>
2021-01-03 21:23:43 -0800

    Finished leetcode 41 problem usign hashmap

--- Java/Coding Challenges/src/LeetCode/FirstMissingPositiveNum41.java
+++ Java/Coding Challenges/src/LeetCode/FirstMissingPositiveNum41.java
@@ -1,10 +1,29 @@
 package LeetCode;
-
+import java.util.*;
 public class FirstMissingPositiveNum41 {
 
+	// Purpose
+	// The purpose of this problem w
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	}
+	
+	public static int hashMapMethod(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for(int i : nums) {
+			if(!map.containsKey(i)) {
+				map.put(i, map.getOrDefault(i, 0) +1);
+			}
+		}
+		
+		for(int i = 0; i < nums.length; i++) {
+			if (!map.containsKey(i+1)){
+				return i+1;
+			}
+		}
+		
+		return nums.length+1;
 	}
 
 }
