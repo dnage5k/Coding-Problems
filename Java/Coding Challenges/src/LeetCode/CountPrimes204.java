e0c337af93e86afb84ebb6bbbb40b525ff93e07a
Dennis <crazydennis2002@yahoo.com>
2021-01-09 08:34:41 -0800

    Finished my notes on prime and problem too

--- Java/Coding Challenges/src/LeetCode/CountPrimes204.java
+++ Java/Coding Challenges/src/LeetCode/CountPrimes204.java
@@ -1,35 +1,40 @@
 package LeetCode;
-
+import java.util.*;
 public class CountPrimes204 {
-
+	// Purpose
+	// The purpose of this problem is to count the numbers of prime given a limit n.
 	public static void main(String[] args) {
-		int i = 12;
+		int i = 5000000;
 		System.out.println(countPrimes(i));
 
 	}
 	
+	// Method
+	// In order to find all prime number count from a given n, we will used sieve method
+	// Sieve method is to create a boolean array containing n amount of number. 
+	// Each index will contain true or false based on if the value is prime or not
+	// false is when the value is prime and we increment count
+	// loops through all multiples of the prime number and set them to true for it be not prime
 	public static int countPrimes(int n) {
         int count = 0;
-        if(n < 1){
+        if(n < 2){
             return count;
-        }else if(n == 2){
-            return 1;
+        }
+        boolean[] isNotPrime = new boolean[n];
+        for(int i = 2; i < n; i++) {
+        	if(!isNotPrime[i]) {
+        		count++;
+        		for(int j = i; j < n; j+=i) {
+        			isNotPrime[j] = true;
+        		}
+        	}
         }
         
-        boolean[] sieve = new boolean[n];
-        for(int i = 0; i < sieve.length; i++){
-            sieve[i] = true;
-        }
+
         
-        for(int i = 2; i < sieve.length; i++){
-            if(sieve[i]){
-                count++;
-                for(int j = i*i; j < n; j += i){
-                    sieve[j] = false;
-                }
-            }
-        }
         return count;
     }
+	
+
 
 }
