8abe3384adfc3a9c10b679798026a8962a6005cf
Dennis <crazydennis2002@yahoo.com>
2021-01-13 21:11:05 -0800

    Finished Missing number with two different solution. Bit manipulation and an easier method to understand.

--- Java/Coding Challenges/src/LeetCode/MissingNumber268.java
+++ Java/Coding Challenges/src/LeetCode/MissingNumber268.java
@@ -4,7 +4,8 @@ import java.util.*;
 public class MissingNumber268 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] nums = {0,2,1,4};
+		System.out.println(bestMethod(nums));
 
 	}
 	
@@ -29,12 +30,29 @@ public class MissingNumber268 {
 	        return -1;
 	    }
 	
+	// Method
+	// 
 	public static int bestMethod(int[] nums) {
+		int n = nums.length;
+		
 		for(int i = 0; i < nums.length; i++) {
-			
+			n ^= (i^nums[i]);
 		}
+		
+		return n;
 	}
 	
+	public static int sumMethod(int[] nums) {
+		int sum = 0;
+		int num = 0;
+		
+		for(int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			num += (i+1);
+		}
+		
+		return(num-sum);
+	}
 
 
 }
