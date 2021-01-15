865db0f22e01400c52bb0470d99f209d094355dd
Dennis <crazydennis2002@yahoo.com>
2021-01-14 22:25:45 -0800

    Added notes on MissingNumber problem

--- Java/Coding Challenges/src/LeetCode/MissingNumber268.java
+++ Java/Coding Challenges/src/LeetCode/MissingNumber268.java
@@ -31,7 +31,13 @@ public class MissingNumber268 {
 	    }
 	
 	// Method
-	// 
+	// I still dont completely understand somebody can get to this solution
+	// I did the math and it works
+	// The idea is we have to use bit manipulation to solve this problem
+	// we first have to create a for loop to iterate through each item of the array
+	// the index XOR with nums[i] then XOR with length of the array will give the missing value if there is one or
+	// the length of the array.
+	
 	public static int bestMethod(int[] nums) {
 		int n = nums.length;
 		
@@ -42,6 +48,13 @@ public class MissingNumber268 {
 		return n;
 	}
 	
+	
+	// Method
+	// To find the missing number, we know that the array nums will contain values from 0 to n
+	// Where n is the length of the array
+	// We can add all the values in the array and that will be the subtraction of to the sum of 0 to n
+	// The subtraction of sum from 0 to n and the sum of the array will reveal the missing number
+	// 
 	public static int sumMethod(int[] nums) {
 		int sum = 0;
 		int num = 0;
