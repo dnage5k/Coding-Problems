22e70cdce4d7a1828cb388739d9530f2a1c65419
Dennis <crazydennis2002@yahoo.com>
2021-01-20 23:11:17 -0800

    Working on minsizeArray problem

---
+++ Java/Coding Challenges/src/LeetCode/MinSizeSubarray209.java
@@ -0,0 +1,59 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class MinSizeSubarray209 {
+/*
+ * 
+ * 
+
+Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+Example: 
+
+Input: s = 7, nums = [2,3,1,2,4,3]
+Output: 2
+Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+
+Follow up:
+If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n). 
+ */
+	public static void main(String[] args) {
+		int s = 11;
+		int nums[] = {1,2,3,4,5};
+		System.out.println(longMethod(s,nums));
+	}
+	
+	public static int longMethod(int s, int[] nums) {
+		int min = Integer.MAX_VALUE;
+		
+		for(int i = 0; i < nums.length; i++) {
+			int sum = 0;
+			int count = 0;
+			for(int j = i; j < nums.length; j++) {
+				if(s >= sum + nums[j]) {
+					count++;
+					sum += nums[j];
+				}
+				
+			}
+			
+			if(s == sum) {
+				min = Math.min(min, count);
+			}
+		}
+		if(min == Integer.MAX_VALUE) {
+			return 0;
+		}else {
+			return min;
+		}
+	}
+	
+	public static int OnlgnMethod(int s, int[] nums) {
+		Arrays.sort(nums);
+		int count = 0;
+		return count;
+		
+	}
+
+}
