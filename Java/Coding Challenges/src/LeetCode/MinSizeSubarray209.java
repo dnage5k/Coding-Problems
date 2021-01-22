3c34b10ba1e7fc9c8387d888e1e7185a3838cb42
Dennis <crazydennis2002@yahoo.com>
2021-01-21 22:43:24 -0800

    Finished long method. I read the question wrong but was able to complete with complete knowledge of the problem.

--- Java/Coding Challenges/src/LeetCode/MinSizeSubarray209.java
+++ Java/Coding Challenges/src/LeetCode/MinSizeSubarray209.java
@@ -1,5 +1,5 @@
 package LeetCode;
-
+import java.util.*;
 import java.util.Arrays;
 
 public class MinSizeSubarray209 {
@@ -7,7 +7,8 @@ public class MinSizeSubarray209 {
  * 
  * 
 
-Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. 
+If there isn't one, return 0 instead.
 
 Example: 
 
@@ -19,32 +20,30 @@ Follow up:
 If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n). 
  */
 	public static void main(String[] args) {
-		int s = 11;
-		int nums[] = {1,2,3,4,5};
+		int s = 7;
+		int nums[] = {2,3,1,2,4,3};
 		System.out.println(longMethod(s,nums));
 	}
 	
 	public static int longMethod(int s, int[] nums) {
 		int min = Integer.MAX_VALUE;
 		
+		
 		for(int i = 0; i < nums.length; i++) {
 			int sum = 0;
 			int count = 0;
 			for(int j = i; j < nums.length; j++) {
-				if(s >= sum + nums[j]) {
-					count++;
-					sum += nums[j];
+				sum += nums[j];
+				count++;
+				if(sum >= s) {
+					min = Math.min(min, count);
+					break;
 				}
-				
-			}
-			
-			if(s == sum) {
-				min = Math.min(min, count);
 			}
 		}
 		if(min == Integer.MAX_VALUE) {
 			return 0;
-		}else {
+		}else{
 			return min;
 		}
 	}
