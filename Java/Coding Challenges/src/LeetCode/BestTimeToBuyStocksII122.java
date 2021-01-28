539f9f58783334ec013b80308ae1d7860ffc6d49
Dennis <crazydennis2002@yahoo.com>
2021-01-27 23:18:19 -0800

    Finished best time to buy stocks 2, added notes with it

--- Java/Coding Challenges/src/LeetCode/BestTimeToBuyStocksII122.java
+++ Java/Coding Challenges/src/LeetCode/BestTimeToBuyStocksII122.java
@@ -3,8 +3,8 @@ package LeetCode;
 public class BestTimeToBuyStocksII122 {
 
 	public static void main(String[] args) {
-		int[] input = {7,1,5,3,6,4};
-		System.out.println(maxProfit(input));
+		int[] input = {7,1,5,6,8,4};
+		System.out.println(longMethod(input));
 	}
 	
 	public static int maxProfit(int[] prices) {
@@ -22,23 +22,27 @@ public class BestTimeToBuyStocksII122 {
 		return max;
 	}
 	
+	// Method
+	// This method from the forum I saw
+	// I couldnt understand when to buy or sell but looking at the solution made me understand this was more of a greedy method
+	// 
 	public static int longMethod(int[] prices) {
+		// int[] input = {7,1,5,3,6,4};
 		int max = 0;
 		int buy = 0;
 		int sell = 0;
-		int[] memo = new int[prices.length];
 		int i = 0;
-		while(i < prices.length) {
-			while(buy > prices[i]) {
+		while(i < prices.length-1) {
+			while(i < prices.length-1 && prices[i+1] <= prices[i]) {
 				i++;
 			}
 			buy = prices[i];
-			while(i < prices.length) {
+			while(i < prices.length-1 && prices[i+1] > prices[i]) {
 				i++;
 			}
 			sell = prices[i];
 			
-			
+			max += sell - buy;
 		}
 		
 		return max;
