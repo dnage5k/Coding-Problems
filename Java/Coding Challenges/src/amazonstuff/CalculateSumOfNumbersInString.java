67003939c96f7c0988707f74f6251f377bb6f5ae
Dennis <crazydennis2002@yahoo.com>
2021-01-30 06:39:58 -0800

    Added notes on checkPairSum

--- Java/Coding Challenges/src/amazonstuff/CalculateSumOfNumbersInString.java
+++ Java/Coding Challenges/src/amazonstuff/CalculateSumOfNumbersInString.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class CalculateSumOfNumbersInString {
 
 	public static void main(String[] args) {
-		String s = "123 342";
+		String s = "123 456";
 		System.out.println(calculateSumOfNumbersInString(s));
 
 	}
@@ -13,7 +13,22 @@ public class CalculateSumOfNumbersInString {
 	   code so that it passes all test cases.
 	   
 	   Purpose:
-	   The purpose of this problem is to add the individual numbers of the string and output the sum of it 
+	   The purpose of this problem is to add the individual numbers of the string and output the sum of it
+	   The problem with the original is that it always output 0
+	   To understand this problem I had to understand what the program was doing
+	   
+	   The for loop loops through the each character of the in the string
+	   it checks if the character is a digit, if it is then it adds that to the String temp
+	   else then we add sum with Integer.parseInt(temp) and equate back to sum
+	   then temp is set to 0
+	   
+	   How to Debug it:
+	   From looking at this, I outputted the answer everytime to see what I would get with the given result and
+	   the result was that sum was always zero for some reason
+	   And the reason was that temp will always reset to zero after the previous statement, hence always returning zero
+	   So what we need to do is put temp ="0" into the else statement so we can reset temp every time to zero when we add it to sum
+	   
+	   
 	 * public int calculateSumOfNumbersInString(String inputString) {
         String temp = "";
         int sum = 0;
--- Java/Coding Challenges/src/amazonstuff/CheckPairSumExists.java
+++ Java/Coding Challenges/src/amazonstuff/CheckPairSumExists.java
@@ -11,13 +11,31 @@ public class CheckPairSumExists {
 
 	}
 	/* ORIGINAL
+	 * The following function returns a boolean value representing if there is a pair with given sum exists in the array.
+	 * 
+	 * Purpose:
+	 * The purpose of this problem is to find one pair that equates to the sum given by 2d array.
+	 * 
+	 * In the original code, there are two for loops that go over the whole array
+	 * The first loop goes through each row and the 2nd loop goes through each column
+	 * if The set contains the sum - arr[i][j] then we return true else we add it to the set
+	 * When the whole loop ends it returns false
+	 * 
+	 * The problem with this code is that its returning true or false when its not that way
+	 * 
+	 * Solution to debug it:
+	 * The reason why its outputting false when it should be true or vice versa is because it has something to do with whats contained in the set
+	 * we would need to change what the set should be adding and what the number we should be checking in the if statement
+	 * the if statement should be changed to set.contains(arr[i][j]) because we want to see if that value has the complement
+	 * the add statement should be set.add(sum-arr[i][j]) because we want to see if the complement is there when we check each index
+	 * 
 	 * public boolean checkPairSumExists(int rows, int cols, int[][] arr, int sum) {
         Set<Integer> set = new HashSet<Integer>();
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
                 if(set.contains(sum - arr[i][j])) {
                     return true;
-                } else {
+                } else{
                     set.add(sum);
                 }
             }
