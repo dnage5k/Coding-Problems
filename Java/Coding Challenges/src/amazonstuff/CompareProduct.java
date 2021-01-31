8f28a1bce5b4f75a54b1fd39d2fa9042f372179b
Dennis <crazydennis2002@yahoo.com>
2021-01-30 21:21:46 -0800

    Finished notes on debugging practice

--- Java/Coding Challenges/src/amazonstuff/CompareProduct.java
+++ Java/Coding Challenges/src/amazonstuff/CompareProduct.java
@@ -10,6 +10,28 @@ public class CompareProduct {
 	}
 	
 	/* ORIGINAL
+	 * 
+	 * Purpose:
+	 * The purpose of this problem is to compare the product of the of the even and odd placement of the given input digits
+	 * 
+	 *Original Code:
+	 * The beginning of the code checks if the input num is less than 10, if it is then return false (its false because there is no tenth digit)
+	 * We create a while loop with condition num > 0
+	 * grab the first digit of num by dividing it by 10 and setting it equal to digit
+	 * multiply digit with the odd variable
+	 * then divide num by 10 to get rid of the last number
+	 * Check to see if num is equal to zero, if it is then we break and return false
+	 * if not then we continue with the same process, getting the individual digit from num and multiplying it with even variable now
+	 * then getting rid of the 2nd digit of num
+	 * The loop continues util num is zero
+	 * 
+	 *  Debugging code:
+	 *  The output is false when 99 was the input even tho it was suppose be true
+	 *  So why is it outputting false even the the individual digits of the num are equal to each other for the end result?
+	 *  This means the digit is wrong to begin, how is it wrong.
+	 *  Well in order to get the last digit of a number we would have to do modulo instead of dividing
+	 *  We divide when we only want to get rid of the digit
+	 * 
 	 * public boolean compareProduct(int num) {
         if (num < 10)
             return false;
--- Java/Coding Challenges/src/amazonstuff/CountTripletSumPermutations.java
+++ Java/Coding Challenges/src/amazonstuff/CountTripletSumPermutations.java
@@ -11,6 +11,24 @@ public class CountTripletSumPermutations {
 	}
 	
 	/* ORIGINAL
+	 * 
+	 * Purpose:
+	 * The purpose of this problem is find how many times given the target sum appears with the product of 3 numbers in the array
+	 * 
+	 * Original Code:
+	 * The code consist of 3 for loops
+	 * The first for loops goes from i = 0 to size -2 where size is the size of the array
+	 * if target sum is a multiplier of the ith index of the array then we move onto the 2nd loop
+	 * The 2nd for loop goes from j = to size -1
+	 * if the target multiplier of the ith and jth index is equal to zero then we move on
+	 * set value equal to the division of target and multiplication of ith and jth index of the array
+	 * last loop finds the kth index of the array if it matches value since its the last multiplier to equate to the target
+	 * 
+	 * Debugging the code:
+	 * the problem starts off by looking at the jth index of the 2nd loop.
+	 * Immediately we know that it shouldnt be zero because it doesnt make sense to check the same index twice so we would need to
+	 * set it to j = i+1
+	 * 
 	 * public int countTripletSumPermutations(int size, int[] arr, int tripletSum)
     {
         int count = 0;
