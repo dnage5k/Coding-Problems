ae0c71791c0d369f3a08355f608914b285a70582
Dennis <crazydennis2002@yahoo.com>
2021-01-30 19:45:24 -0800

    Finishing up practice notes on debugging problems

--- Java/Coding Challenges/src/amazonstuff/RemoveConVowels.java
+++ Java/Coding Challenges/src/amazonstuff/RemoveConVowels.java
@@ -8,6 +8,28 @@ public class RemoveConVowels {
 
 	}
 	/* ORIGINAL
+	 * 
+	 * Purpose:
+	 * The purpose of this problem is to find and fix the error in the algorithm. The algo works by returning a non-consecutive vowel string
+	 * For example, if the input string is aaba then the output should be aba. The algo should not be returning any consecutive vowel
+	 * 
+	 * Original Code:
+	 * In the original code, we have a helper boolean function called is_vowel which checks if the character is a vowel or not
+	 * Then we have the main function that returns the string without consecutive vowels
+	 * We have a for loop that starts at index 1 since the beginning of the function starts the new string as the first index of the input string
+	 * in the loop, theres an if statement condition that states if the character i-1 in the string is not a vowel and the current index character 
+	 * is not a vowel too then we add it to str1
+	 * We return the final string
+	 * 
+	 * Debugging the code:
+	 * The normal thing to do in an amazon coding assessment is to output the answer to see what is expected and what was the actual output
+	 * What was expected was "aba" but what output was "a" given the input string was "aba"
+	 * It seems that the string is only added when the if statement is true which means it has something to do with the if statement
+	 * The original if statement states: if((!is_vowel(str.charAt(i-1))) && (!is_vowel(str.charAt(i)))) which means the if statement is only true
+	 * if the character at i-1 is not a vowel AND current i is not a vowel then its true
+	 * The problem is that it will never be true unless both the previous char and current char is not a vowel
+	 * We need to change the conditional statement to OR because in order to add a non vowel into a string then we put OR conditional
+	 * 
 	 * boolean is_vowel(char ch) {
         return (ch == 'a') || (ch == 'e') ||
                 (ch == 'i') || (ch == 'o') ||
--- Java/Coding Challenges/src/amazonstuff/ReverseAlphabetCharsOnly.java
+++ Java/Coding Challenges/src/amazonstuff/ReverseAlphabetCharsOnly.java
@@ -7,28 +7,51 @@ public class ReverseAlphabetCharsOnly {
 		System.out.println(reverseAlphabetCharsOnly(s));
 	}
 	
-	public static String reverseAlphabetCharsOnly(String inputString) {
-		/* ORIGINAL
-		 * public String reverseAlphabetCharsOnly(String inputString) {
-        char[] inputChar = inputString.toCharArray();
-        int right = inputString.length() - 1;
-        int left = 0;
-        while(left < right) {
-            if(!Character.isAlphabetic(inputChar[left]))
-                left++;
-            else if(!Character.isAlphabetic(inputChar[right]))
-                right--;
-            else {
-                char temp = inputChar[left];
-                inputChar[left] = inputChar[right];
-                inputChar[right] = temp;
-            }
+	/* ORIGINAL
+	 * Purpose:
+	 * The purpose of the this problem is to reverse the given string but only reverse alphabetic characters and ignore the rest
+	 * 
+	 * Original code:
+	 * In the original code, we convert the string to an char array
+	 * then iterate through the char array with having a left pointer and a right pointer
+	 * the while loop continues when the left pointer is less than the right pointer
+	 * in the loop, you have two if statements. The first statement checks to see if the left index of the array is not an alphabet
+	 * if it is not an alphabet, then we increment the left pointer since we only care about reversing only alphabet
+	 * the next if statement checks the same thing but with the right pointer
+	 * if none of if statements passes then we hit the else statement where we switch both of the character of the left and right pointer
+	 * exiting the conditional if statements, we increment both the left and right pointer
+	 * 
+	 * Debugging the code:
+	 * When I output the code, it appears that not all the characters were flipped even though it should have which means the pointers
+	 * are skipping
+	 * I would have to look at why are the pointers skipping which consist of the if statements and the ending statements
+	 * The if statements made sense since its only incrementing when its not an alphabet which what we want. So both statements are fine
+	 * The ending of the while loop is wrong. Why? Because it is incrementing left and right pointer regardless of if it is a special character
+	 * What we need to do is increment the left and right pointer after we do the swapping in the else statement
+	 * This way, it is not going to skip any characters
+	 * 
+	 * public String reverseAlphabetCharsOnly(String inputString) {
+    char[] inputChar = inputString.toCharArray();
+    int right = inputString.length() - 1;
+    int left = 0;
+    while(left < right) {
+        if(!Character.isAlphabetic(inputChar[left]))
             left++;
+        else if(!Character.isAlphabetic(inputChar[right]))
             right--;
+        else {
+            char temp = inputChar[left];
+            inputChar[left] = inputChar[right];
+            inputChar[right] = temp;
         }
-        return new String(inputChar);
+        left++;
+        right--;
     }
-		 */
+    return new String(inputChar);
+}
+	 */
+	public static String reverseAlphabetCharsOnly(String inputString) {
+		
         char[] inputChar = inputString.toCharArray();
         int right = inputString.length() - 1;
         int left = 0;
