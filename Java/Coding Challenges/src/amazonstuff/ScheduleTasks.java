2e296542fca374690ee1da6bcd2e4dc48e269645
Dennis <crazydennis2002@yahoo.com>
2021-02-02 05:51:17 -0800

    Working on sheduleTasks

---
+++ Java/Coding Challenges/src/amazonstuff/ScheduleTasks.java
@@ -0,0 +1,24 @@
+package amazonstuff;
+import java.util.*;
+public class ScheduleTasks {
+
+	public static void main(String[] args) {
+		int num = 5;
+		int[] power = {4,2,8,3,5};
+		int tasks = 19;
+
+	}
+	
+	public static int scheduleTasks(int num, int[] power, int tasks) {
+		int sec = 0;
+		List<Integer> list = new ArrayList<>();
+		for(int i = 0; i < power.length; i++) {
+			list.add(power[i]);
+		}
+		
+		while(tasks > 0) {
+			
+		}
+	}
+
+}
