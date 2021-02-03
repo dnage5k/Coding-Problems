9690a4933e59b8851a2ff88e61f3084dff9d0f3d
Dennis <crazydennis2002@yahoo.com>
2021-02-03 05:57:39 -0800

    Finished half of scheduleTasks

--- Java/Coding Challenges/src/amazonstuff/ScheduleTasks.java
+++ Java/Coding Challenges/src/amazonstuff/ScheduleTasks.java
@@ -6,19 +6,34 @@ public class ScheduleTasks {
 		int num = 5;
 		int[] power = {4,2,8,3,5};
 		int tasks = 19;
-
+		System.out.println(scheduleTasks(num,power,tasks));
 	}
 	
+	// time complexity O(nlogn) space: O(n)
 	public static int scheduleTasks(int num, int[] power, int tasks) {
 		int sec = 0;
+		Arrays.sort(power);
 		List<Integer> list = new ArrayList<>();
 		for(int i = 0; i < power.length; i++) {
 			list.add(power[i]);
 		}
-		
+		int i = 0;
+		int taskNum = tasks;
 		while(tasks > 0) {
+			if(list.contains(taskNum)) {
+				tasks -= list.get(list.indexOf(taskNum));
+				sec++;
+			}
+			if(list.get(i) < list.get(i+1)) {
+				i++;
+			}
+			tasks -= list.get(i);
+			list.set(i, (list.get(i)/2));
+			sec++;
 			
 		}
+		
+		return sec;
 	}
 
 }
