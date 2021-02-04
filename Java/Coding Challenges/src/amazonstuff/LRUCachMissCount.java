e351d0010a6b6e52ff5a16913251166fb5262d64
Dennis <crazydennis2002@yahoo.com>
2021-02-04 05:15:18 -0800

    Finished LRUcacheMissCount problem. Cleaned up code too on the same problem.

--- Java/Coding Challenges/src/amazonstuff/LRUCachMissCount.java
+++ Java/Coding Challenges/src/amazonstuff/LRUCachMissCount.java
@@ -3,12 +3,35 @@ import java.util.*;
 public class LRUCachMissCount {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int maxCacheSize = 5;
+		int num = 6;
+		List<Integer> pages = List.of(1, 6, 3, 7, 1, 9, 3, 6, 4, 8);
+		System.out.println(lruCacheMiss(num,pages,maxCacheSize));
 
 	}
 	
+	// You can only implement ENQUEUE and DEQUEUE using a linkedlist
 	public static int lruCacheMiss(int num, List<Integer> pages, int maxCacheSize) {
 		int count = 0;
+		LinkedList<Integer> que = new LinkedList<>();
+		for(int i = 0; i < pages.size(); i++) {
+			if(!que.contains(pages.get(i))) {
+				if(que.size() < maxCacheSize) {
+					count++;
+				}else {
+					que.remove();
+					count++;
+				}
+			}else{
+				que.remove(que.get(que.indexOf(pages.get(i))));
+			}
+			
+					
+			que.add(pages.get(i));
+					
+				
+			}
+	
 		
 		return count;
 	}
