97cc79380fe48130011125cd0438ebd08e7e352f
Dennis <crazydennis2002@yahoo.com>
2021-02-11 23:58:19 -0800

    added objects to test

--- Java/Coding Challenges/src/amazonstuff/LargestDeficit.java
+++ Java/Coding Challenges/src/amazonstuff/LargestDeficit.java
@@ -4,11 +4,60 @@ import java.util.Map.Entry;
 public class LargestDeficit {
 
 	public static void main(String[] args) {
-		List<debtRecord> records = new ArrayList<>();
-		List<debtRecord> importers = new ArrayList<>();
 		
-		importers.add(null);
-		System.out.println(importers);
+		List<debtRecord> importers = new ArrayList<>();
+		debtRecord rec1 = new debtRecord();
+		debtRecord rec2 = new debtRecord();
+		debtRecord rec3 = new debtRecord();
+		debtRecord rec4 = new debtRecord();
+		debtRecord rec5 = new debtRecord();
+		debtRecord rec6 = new debtRecord();
+		debtRecord rec7 = new debtRecord();
+		debtRecord rec8 = new debtRecord();
+		debtRecord rec9 = new debtRecord();
+		debtRecord rec10 = new debtRecord();
+		
+		rec1.borrower = "Jeff";
+		rec1.lender = "Ivy";
+		rec1.amount = 3;
+
+		rec2.borrower = "Jeff";
+		rec2.lender = "Greg";
+		rec2.amount = 6;
+		
+		rec3.borrower = "Ivy";
+		rec3.lender = "Casey";
+		rec3.amount = 5;
+
+		rec4.borrower = "Ellen";
+		rec4.lender = "Casey";
+		rec4.amount = 4;
+		
+		rec5.borrower = "Alex";
+		rec5.lender = "Jeff";
+		rec5.amount = 8;
+		
+		rec6.borrower = "Ivy";
+		rec6.lender = "Ellen";
+		rec6.amount = 8;
+		
+		rec7.borrower = "Ivy";
+		rec7.lender = "Casey";
+		rec7.amount = 4;
+		
+		rec8.borrower = "Ivy";
+		rec8.lender = "Alex";
+		rec8.amount = 2;
+		
+		rec9.borrower = "Frank";
+		rec9.lender = "Ivy";
+		rec9.amount = 8;
+		
+		rec10.borrower = "Casey";
+		rec10.lender = "David";
+		rec10.amount = 4;
+		List<debtRecord> records = List.of(rec1,rec2,rec3,rec4,rec5,rec6,rec7,rec8,rec9,rec10);
+		System.out.println(minimumDebtMembers(records));
 	}
 	
 	// Method
@@ -59,6 +108,8 @@ public class LargestDeficit {
 	
 
 }
+
+// If no constructor is specify then it has one automatically as an empty constructor
  class debtRecord{
     public  String borrower = ""; // importer
     public  String lender = ""; //exporter
