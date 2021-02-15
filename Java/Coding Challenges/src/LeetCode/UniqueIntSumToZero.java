c3b06d163d66e4fb14f8b35b2a5dd111d0dfe722
Dennis <crazydennis2002@yahoo.com>
2021-02-14 21:13:24 -0800

    Finished notes

--- Java/Coding Challenges/src/LeetCode/UniqueIntSumToZero.java
+++ Java/Coding Challenges/src/LeetCode/UniqueIntSumToZero.java
@@ -2,12 +2,23 @@ package LeetCode;
 import java.util.*;
 public class UniqueIntSumToZero {
 
+//	 Purpose
+//	 The purpose of this problem is to return an array given size n that sums to
+//   zero when added together
 	public static void main(String[] args) {
 		int n = 5;
 		System.out.println(Arrays.toString(solution(n)));
 
 	}
 	
+	 /*Method
+	 create an array with size n
+	 create two variables i (the index of the array so we could increment) and num (the number we increment and insert into the array)
+	 check to see if n is an odd or even, if its odd then let i = 1 and decrement n else let i = 0
+	 
+	 loop through the whole array until n is equal to zero while inserting the variable num into each
+	 index and incrementing everytime.
+	*/
 	public static int[] solution(int n) {
 		int[] arr = new int[n];
 		int i;
