ed269835e4f4a1020fb4536aa329bab6d9124c52
Dennis <crazydennis2002@yahoo.com>
2021-02-17 03:59:17 -0800

    Finishing storage optimization problem

---
+++ Java/Coding Challenges/src/LeetCode/MaxAreaHandV.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+public class MaxAreaHandV {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        int max = 0;
+        
+        return max;
+    }
+
+}
