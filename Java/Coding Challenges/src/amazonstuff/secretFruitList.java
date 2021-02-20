7736e4ac683f47737f289bf38d640bc2221fadce
Dennis <crazydennis2002@yahoo.com>
2021-02-19 21:50:02 -0800

    Working on fruit list algo

--- Java/Coding Challenges/src/amazonstuff/secretFruitList.java
+++ Java/Coding Challenges/src/amazonstuff/secretFruitList.java
@@ -5,14 +5,25 @@ public class secretFruitList {
 	public static void main(String[] args) {
 		List<String> list1 = List.of("orange", "mango");
 		List<String> list2 = List.of("watermelon", "mango");
+		List<String> custList = List.of("orange", "mango", "watermelon", "mango");
 		List<List<String>> fruitList = List.of(list1,list2);
+		System.out.println(matchSecretLists(fruitList,custList));
 
 	}
 	
 	public static boolean matchSecretLists(List<List<String>> secretFruitList, List<String> customerPurchasedList) {
 		boolean ans = true;
-		
-		return ans;
+		int p = 0;
+		for(int i = 0; i < secretFruitList.size(); i++) {
+			
+			for(int j = 0; j < secretFruitList.get(i).size(); j++) {
+				if(!customerPurchasedList.get(p).equals(secretFruitList.get(i).get(j))) {
+					return false;
+				}
+				p++;
+			}
+		}
+		return true;
 		
 		
     }
