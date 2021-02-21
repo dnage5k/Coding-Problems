dff68c45f8989f81c86ce5bfb6cf95ee810569c4
Dennis <crazydennis2002@yahoo.com>
2021-02-20 19:44:32 -0800

    skipping fruit basket problem for now and working on linked list problem

--- Java/Coding Challenges/src/amazonstuff/secretFruitList.java
+++ Java/Coding Challenges/src/amazonstuff/secretFruitList.java
@@ -19,19 +19,60 @@ public class secretFruitList {
 	 
 	 
 	public static boolean matchSecretLists(List<List<String>> secretFruitList, List<String> customerPurchasedList) {
+		boolean state = true;
+		boolean ans = true;
 		int p = 0;
-		for(int i = 0; i < secretFruitList.size(); i++) {
-			
-			for(int j = 0; j < secretFruitList.get(i).size(); j++) {
-				if(!customerPurchasedList.get(p).equals(secretFruitList.get(i).get(j))) {
-					return false;
-				}
-				p++;
+
+		// 3 pointers to iterate through two arrays
+		// one pointer iterates through the list of secret list, another pointer iterates within the secret list and one that iterates through the customer's list
+		
+		// let k be the one iterate through the customer list while j iterate within the secret Fruit List 
+		int k = 0 ;
+		int j = 0;
+		int i = 0;
+		while(i < secretFruitList.size() &&  state) {
+			if(secretFruitList.get(i).get(j) == "anything" || secretFruitList.get(i).get(j) == customerPurchasedList.get(k)) {
+				k++;
+				j++;
+			}else{
+				k++;
 			}
+			
+			if(j == secretFruitList.get(i).size()) {
+				i++;
+				j = 0;
+			}
+			
 		}
 		return true;
 		
 		
     }
+	
+	public static boolean sol(List<List<String>> secretFruitList, List<String> customerPurchasedList) {
+		 if(secretFruitList == null || secretFruitList.size() == 0)
+		        return true;
+		    if(customerPurchasedList == null || customerPurchasedList.size() == 0)
+		        return false;
+		    int i=0,j=0;
+		    for(int k=0;k<customerPurchasedList.size();k++) {
+		        //when match success
+		        if(secretFruitList.get(i).get(j).equals(customerPurchasedList.get(k)) || secretFruitList.get(i).get(j).equals("anything")) {
+		            j++;
+		            if(j == secretFruitList.get(i).size()) {
+		                i++;
+		                j=0;
+		            }
+		            if(i == secretFruitList.size())
+		                return true;
+		        }else {
+		            //when match failed, k and j both go back.
+		            k-=j;
+		            j=0;
+		        }
+		    }
+		    return false;
+
+	}
 
 }
