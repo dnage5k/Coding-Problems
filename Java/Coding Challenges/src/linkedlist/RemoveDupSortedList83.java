580f732ee683b775948f148595228ccf606845cc
Dennis <crazydennis2002@yahoo.com>
2021-02-22 01:03:27 -0800

    Working on removeDupSorted for linkedList

---
+++ Java/Coding Challenges/src/linkedlist/RemoveDupSortedList83.java
@@ -0,0 +1,18 @@
+package linkedlist;
+import datastructure.ListNode;
+public class RemoveDupSortedList83 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static ListNode sol(ListNode head) {
+		ListNode headAns = new ListNode();
+		ListNode curr = headAns;
+		
+		
+		return headAns;
+	}
+
+}
