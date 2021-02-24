d5289dc183c33760f1835ed040e3a374936a9670
Dennis <crazydennis2002@yahoo.com>
2021-02-24 04:52:13 -0800

    Finished middle of linked list problem

--- Java/Coding Challenges/src/linkedlist/MiddleOfLinkedList876.java
+++ Java/Coding Challenges/src/linkedlist/MiddleOfLinkedList876.java
@@ -3,10 +3,21 @@ import datastructure.ListNode;
 public class MiddleOfLinkedList876 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ListNode head = new ListNode(1);
+		ListNode curr = head;
+		curr.next = new ListNode(2);
+		curr = curr.next;
+		curr.next = new ListNode(3);
+		curr = curr.next;
+		curr.next = new ListNode(4);
+		curr = curr.next;
+		curr.next = new ListNode(5);
+		curr = curr.next;
+		
+		System.out.println(sol(head));
 
 	}
-	public static ListNode sol1(ListNode head) {
+	public static ListNode sol(ListNode head) {
 		int count = 0;
 		ListNode curr = head;
 		while(curr != null) {
@@ -20,14 +31,17 @@ public class MiddleOfLinkedList876 {
 		ListNode newCurr = newNode;
 		int newCount = 0;
 		while(curr != null) {
-			if(count/2 < newCount) {
-				newCurr = curr;
-				newCurr.next = new ListNode();
+			if(count/2 <= newCount) {
+				System.out.println(curr.val);
+				newCurr.next = new ListNode(curr.val);
+				newCurr = newCurr.next;
 				
 			}
 			newCount++;
 			curr = curr.next;
 		}
+		
+		return newNode.next;
 	}
 
 }
