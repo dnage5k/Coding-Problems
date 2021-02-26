16c412ea9f3b632e94567ef24d385f80e412cb33
Dennis <crazydennis2002@yahoo.com>
2021-02-26 10:19:56 -0800

    Working on intersection of two linked list

---
+++ Java/Coding Challenges/src/linkedlist/IntersectionofTwoLinkedLists160.java
@@ -0,0 +1,19 @@
+package linkedlist;
+
+import datastructure.ListNode;
+
+public class IntersectionofTwoLinkedLists160 {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode intersect = new ListNode();
+        
+        
+        return intersect.next;
+    }
+
+}
