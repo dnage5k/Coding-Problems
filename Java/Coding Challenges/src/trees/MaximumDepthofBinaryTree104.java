fafa0dc7b2603f6d5a62e69ddca81482a7eb8718
Dennis <crazydennis2002@yahoo.com>
2021-03-05 23:05:44 -0800

    Working on LC 104

---
+++ Java/Coding Challenges/src/trees/MaximumDepthofBinaryTree104.java
@@ -0,0 +1,14 @@
+package trees;
+import datastructure.TreeNode;
+public class MaximumDepthofBinaryTree104 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int maxDepth(TreeNode root) {
+        int depth = 0;
+        return depth;
+    }
+}
